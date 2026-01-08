e354d2d0e0517c7ce67d2f59a56da796b2840e1d
Dũng TT <dungtienthe1920@gmail.com>
2026-01-08 21:12:53 +0700

    feat: add StatDefinitions

---
+++ Templates/StatDefinition.cs
@@ -0,0 +1,17 @@
+﻿using Crimson_Knight_Server.Stats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crimson_Knight_Server.Templates
+{
+    public class StatDefinition
+    {
+        public StatId StatId { get; set; }
+        public string Name { get; init; }
+        public string Description { get; init; }
+        public bool IsPercent { get; init; }
+    }
+}
--- Templates/TemplateManager.cs
+++ Templates/TemplateManager.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Crimson_Knight_Server.Stats;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -15,6 +17,9 @@ namespace Crimson_Knight_Server.Templates
         public static List<DepartTemplate> DepartTemplates = new List<DepartTemplate>();
         public static List<NpcTemplate> NpcTemplates = new List<NpcTemplate>();
 
+        private static Dictionary<StatId, StatDefinition> StatDefinitions;
+        public static StatDefinition GetStatDefinition(StatId id)
+            => StatDefinitions.TryGetValue(id, out var def) ? def : null;
 
         static string DataDirectory = "Resources";
         public static void LoadTemplate()
@@ -23,6 +28,24 @@ namespace Crimson_Knight_Server.Templates
             MonsterTemplates = LoadTemplates<MonsterTemplate>("MonsterTemplates.json");
             MapTemplates = LoadTemplates<MapTemplate>("MapTemplates.json");
             DepartTemplates = LoadTemplates<DepartTemplate>("DepartTemplates.json");
+
+            LoadStats();
+        }
+
+        private static void LoadStats()
+        {
+            string fileName = "StatDefinitions.json";
+            string filePath = Path.Combine(DataDirectory, fileName);
+            string jsonString = File.ReadAllText(filePath);
+
+            var list = JsonSerializer.Deserialize<List<StatDefinition>>(jsonString,
+                new JsonSerializerOptions
+                {
+                    Converters = { new JsonStringEnumConverter() },
+                    PropertyNameCaseInsensitive = true
+                });
+
+            StatDefinitions = list.ToDictionary(s => s.StatId, s => s);
         }
 
         private static List<T> LoadTemplates<T>(string fileName)
